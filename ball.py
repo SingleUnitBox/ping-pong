a88a3e779dcc0faaa40c328b28eeff89d2483f03
SingleUnitBox <cezaryzukowski90@gmail.com>
2022-09-24 11:58:31 +0100

    class Ball added

---
+++ ball.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.x_move = 20
+        self.y_move = 20
+
+    def move(self):
+        x = self.xcor() + self.x_move
+        y = self.ycor() + self.y_move
+        self.goto(x, y)
+
+    def bounce(self):
+        self.y_move *= -1
+
+
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
 import time
 
 right_paddle = Paddle(370)
 left_paddle = Paddle(-370)
+ball = Ball()
 
 screen = Screen()
 screen.bgcolor("black")
@@ -14,12 +16,19 @@ screen.listen()
 screen.onkeypress(key="Up", fun=right_paddle.move_up)
 screen.onkeypress(key="Down", fun=right_paddle.move_down)
 
+screen.onkeypress(key="w", fun=left_paddle.move_up)
+screen.onkeypress(key="s", fun=left_paddle.move_down)
+
 
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
+
+    ball.move()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
 
 
 
